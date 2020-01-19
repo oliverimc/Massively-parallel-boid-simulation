343d6b1b34223d1064eaedbf5ec7b9feb444afdd
Oliver Cohen <ollie.oliver@live.co.uk>
2020-01-19 13:42:56 +0000

    replaced macro values with constexpr

--- boid_final_project/preprocessor.h
+++ boid_final_project/preprocessor.h
@@ -1,28 +1,25 @@
 #pragma once
 
-#define THREAD_NUM 4
-#define MASTER 0 
-#define SAVE false
+constexpr auto THREAD_NUM = 4;
+constexpr auto MASTER = 0 ;
+constexpr auto SAVE = false;
+
+constexpr auto BUFFER_FRACTION = 2 ;
+
+constexpr auto LENGTH = 1000;
+constexpr auto SEEING_DISTANCE = 50;
+constexpr auto SEEING_DISTANCE_SQ = SEEING_DISTANCE * SEEING_DISTANCE;
+constexpr auto BOID_NUMBER = 1000;
+constexpr auto STEPS = 1000;
+
+constexpr auto MAX_SPEED = 3.0;
+constexpr auto MAX_FORCE = 0.6;
+
+constexpr auto COHESION_FACTOR = 1;
+constexpr auto ALIGNMENT_FACTOR = 1;
+constexpr auto SEPARATION_FACTOR = 1.05;
+
 #define SCHEDULE guided
-#define BUFFER_FRACTION 2 
-
-#define LENGTH 1000
-#define SEEING_DISTANCE 200
-#define SEEING_DISTANCE_SQ SEEING_DISTANCE*SEEING_DISTANCE
-#define BOID_NUMBER 1000
-#define STEPS 1000
-
-#define MAX_SPEED 3
-#define MAX_FORCE 0.6
-
-#define PI 3.14159265359
-
-
-#define COHESION_FACTOR 1
-#define ALIGNMENT_FACTOR 1
-#define SEPARATION_FACTOR 1.05
-
-
 #define print(val) std::cout<< val << std::endl;
 #define PathIndice(boid,step,boid_number) step*boid_number+boid
 #define MultiPathIndice(boid,step,boid_number,start) step*boid_number+(boid-start)
