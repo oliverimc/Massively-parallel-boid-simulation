890cdeb965288caf144d12c5ab04833e2fc4c876
Oliver Cohen <ollie.oliver@live.co.uk>
2020-02-11 20:01:38 +0000

    refactoring spacing

--- boid_final_project/worker.cpp
+++ boid_final_project/worker.cpp
@@ -2,26 +2,22 @@
 #include "worker.h"
 
 /*! \file worker.cpp
-	\brief Implementation of function for running simulation on a worker node
-
-
+	\brief Implementation of function for running simulation on a worker node.
 */
 
 using namespace std;
 using namespace Eigen;
 
-
 /**
- * \brief   Main function for executing simulation on a worker node 
+ * \brief  Main function for executing simulation on a worker node.
  * \param  rank | MPI node rank
  * \param  size | Number of MPI ranks
- * \return  | Position data for each step of the simulations for the workers portion of the boids.
+ * \return  | Position data for each step of the simulations for the workers portion of the boids
  */
 vector<Vector3f> run_worker(int rank, int size)
 {
-
 	vector<Boid> boids(BOID_NUMBER);
-	vector<float> boid_memory(BOID_NUMBER * 6);
+	vector<float> boid_memory(BOID_NUMBER * SYS_DIM * 2);
 	vector<int> grid_updates; //vector representing an update to the grid. Each update adds three integers: old spatial grid vector index, new grid vector index, boids vector index
 
 	int boids_per_node = floor(BOID_NUMBER / size);
@@ -29,36 +25,24 @@ vector<Vector3f> run_worker(int rank, int size)
 	int end_index = start_index + boids_per_node;
 	
 	vector<Vector3f> paths(boids_per_node*STEPS);
-	vector<float> node_boid_memory(boids_per_node * 6);
+	vector<float> node_boid_memory(boids_per_node * SYS_DIM * 2);
 
 	BroadcastReceiveBoids(boids, boid_memory, MASTER);
 
 	SpatialGrid grid(boids);
 
-	
-	
 	double start_t = MPI_Wtime();
 	for (int step = 0; step < STEPS; step++)
 	{
 		grid_updates.resize(0);
-		
-		
-
+	
 		#pragma omp parallel for schedule(SCHEDULE)
 		for (int boid = start_index; boid < end_index; boid++)
 		{
-
 			grid.UpdateNearCells(boids[boid]);
 			boids[boid].Update();
-			if (!(MultiPathIndice(boid, step, boids_per_node, start_index) < paths.size()))
-			{
-				printf("%d ITS FUCKED\n", rank);
-			}
 			paths[MultiPathIndice(boid, step, boids_per_node, start_index)] = boids[boid].GetPosition();
-
 		}
-	
-
 		for (int boid = start_index; boid < end_index; boid++)
 		{
 			if (grid.UpdateGrid(boids[boid], grid_updates,size))
@@ -66,35 +50,22 @@ vector<Vector3f> run_worker(int rank, int size)
 				grid_updates.push_back(boid);
 			}
 		}
-	
-
+		
+		//Send updated boids and grid to master	
 		SendBoids(boids, node_boid_memory, MASTER, start_index, end_index);
 		SendGridUpdates(grid_updates, MASTER);
 		
-
+		//Receive updates from other nodes via master
 		BroadcastReceiveGridUpdates(grid_updates, MASTER);
 		BroadcastReceiveBoids(boids, boid_memory, MASTER);
-
-	
-
-
-
+			   		 
 		for (int i = 0; i < grid_updates.size(); i += 3)
 		{
-			
 			grid.UpdateGrid(boids[grid_updates[i + 2]], grid_updates[i], grid_updates[i + 1]);
-
 		}
-
 	}
 	double end_t = MPI_Wtime();
 
-
-
-
 	return paths;
-
-
-
 
 }
